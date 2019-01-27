ee502a4c7bc1b427d25f0b80a90c2b62685c6c1d
Andrey <folganoid@yahoo.com>
2019-01-27 20:39:47 +0200

    struct

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #g++ -o a.sh main.cpp -lGL -lGLU -lglfw -lrt -lm -ldl -lXrandr -lXinerama -lXcursor -lXext -lXrender -lXfixes -lX11 -lpthread -lxcb -lXau -lXdmcp -lGLEW -lGLU -lGL -lm -ldl -ldrm -lXdamage -lX11-xcb -lxcb-glx -lxcb-dri2 -lxcb-dri3 -lxcb-present -lxcb-sync -lxshmfence -lXxf86vm -lXfixes -lXext -lX11 -lpthread -lxcb -lXau -lXdmcp
-g++ -o a.sh ConsoleApp1.cpp -lGL -lGLU -lglfw -lglut -lrt -lm -ldl -lXrandr -lXinerama -lXcursor -lXext -lXrender -lXfixes -lX11 -lpthread -lxcb -lXau -lXdmcp -lGLEW -lGLU -lGL -lm -ldl -ldrm -lXdamage -lX11-xcb -lxcb-glx -lxcb-dri2 -lxcb-dri3 -lxcb-present -lxcb-sync -lxshmfence -lXxf86vm -lXfixes -lXext -lX11 -lpthread -lxcb -lXau -lXdmcp
+g++ -o a.sh snake.cpp
 ./a.sh
